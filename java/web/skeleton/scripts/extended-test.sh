9c7da001e033c41ac95def890cf364039373fbd2
Tiago Alves <tmcalves@gmail.com>
2024-09-25 13:58:57 +0100

    Tweak templates for reliability

---
+++ java/web/skeleton/scripts/extended-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+subenv=$1
+tenant_name=$2
+app_name=${3:-$tenant_name}
+scale_down=${4:-false}
+timeout=${5:-"3m"}
+
+POD_NAME=${app_name}-${subenv}-test
+
+./helm-test.sh executeTests $subenv $tenant_name $app_name false $timeout $tenant_name-$subenv-test
+
+./helm-test.sh executeTests $subenv $tenant_name $app_name $scale_down $timeout $tenant_name-$subenv-test-validate
+
