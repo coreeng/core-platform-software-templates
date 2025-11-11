0b24af2f4683c1dd85f8274cc666c8236013a5c0
Konstantinos Konstantinidis <konstantinos.konstantinidis@cecg.io>
2025-11-11 18:23:58 +0000

    Update infra-cloudsql

--- infra/cloudsql/skeleton/infrastructure/code/modules/cloudsql/locals.tf
+++ infra/cloudsql/skeleton/infrastructure/code/modules/cloudsql/locals.tf
@@ -1,17 +1,26 @@
 locals {
-  # map postgresql clusters by name for easy for_each with automatic IAM authentication flag
+  # map postgresql clusters by name for easy for_each with automatic IAM authentication and audit flags
   postgresql_clusters_map = {
     for r in var.cloudsql.clusters.postgresql : r.name => merge(r, {
-      # Automatically add cloudsql.iam_authentication flag if any database has iam_users configured
-      database_flags = length(flatten([for db in r.databases : db.iam_users])) > 0 ? concat(
-        # Add the IAM authentication flag if not already present
-        contains([for flag in r.database_flags : flag.name], "cloudsql.iam_authentication") ? [] : [{
+      # Automatically add database flags based on configuration
+      database_flags = concat(
+        # Add the IAM authentication flag if any database has iam_users configured and flag not already present
+        length(flatten([for db in r.databases : db.iam_users])) > 0 && !contains([for flag in r.database_flags : flag.name], "cloudsql.iam_authentication") ? [{
           name  = "cloudsql.iam_authentication"
           value = "on"
-        }],
+        }] : [],
+        # Add audit flags if audit is enabled and flags not already present
+        r.audit_config.enabled && !contains([for flag in r.database_flags : flag.name], "cloudsql.enable_pgaudit") ? [{
+          name  = "cloudsql.enable_pgaudit"
+          value = "on"
+        }] : [],
+        r.audit_config.enabled && !contains([for flag in r.database_flags : flag.name], "pgaudit.log") ? [{
+          name  = "pgaudit.log"
+          value = r.audit_config.log_statement_classes
+        }] : [],
         # Include all user-defined flags
         r.database_flags
-      ) : r.database_flags
+      )
       # Collect all unique IAM users from all databases for this cluster
       iam_users = distinct(flatten([for db in r.databases : db.iam_users]))
     })
