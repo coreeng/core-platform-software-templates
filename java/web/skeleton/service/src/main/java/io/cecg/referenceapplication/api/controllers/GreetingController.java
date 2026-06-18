04ba6713619fc97b6b569d9fc2ff398d9a9fd213
Konstantinos Konstantinidis <kkonstan@users.noreply.github.com>
2026-06-18 07:47:46 +0100

    Escape Java greeting name (#160)

--- java/web/skeleton/service/src/main/java/io/cecg/referenceapplication/api/controllers/GreetingController.java
+++ java/web/skeleton/service/src/main/java/io/cecg/referenceapplication/api/controllers/GreetingController.java
@@ -3,11 +3,12 @@ package io.cecg.referenceapplication.api.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 @RestController
 public class GreetingController {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        return String.format("Hello %s!", HtmlUtils.htmlEscape(name));
     }
 }
