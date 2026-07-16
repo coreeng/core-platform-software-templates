e37e5a9fbccb976257bcdcd8933840ec616217ea
Konstantinos Konstantinidis <konstantinos.konstantinidis@cecg.io>
2026-07-16 18:10:54 +0100

    Sync generated template dependency updates

--- infra/urlrouter/skeleton/infrastructure/code/versions.tf
+++ infra/urlrouter/skeleton/infrastructure/code/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.37.0"
+      version = "7.40.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "7.37.0"
+      version = "7.40.0"
     }
     random = {
       source  = "hashicorp/random"
